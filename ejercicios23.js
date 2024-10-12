51ea77a40c8003d0a9074286fd623189af5db67e
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-11 22:17:51 -0600

    Create ejercicios23.js

---
+++ ejercicios23.js
@@ -0,0 +1,8 @@
+public class RedondearNumero {
+    public static void main(String[] args) {
+        double numero = 3.14159;
+        long resultado = Math.round(numero);
+
+        System.out.println("Número redondeado: " + resultado);
+    }
+}
