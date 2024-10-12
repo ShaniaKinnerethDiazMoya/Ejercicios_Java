4311a4b8d1b0d40f687b318ad5dc4ade7d63ceca
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-11 22:17:14 -0600

    Create ejercicios22.js

---
+++ ejercicios22.js
@@ -0,0 +1,8 @@
+public class RaizCuadrada {
+    public static void main(String[] args) {
+        double numero = 25;
+        double resultado = Math.sqrt(numero);
+
+        System.out.println("La raíz cuadrada de " + numero + " es: " + resultado);
+    }
+}
