15ac2ba30b1ce9652220066808aa45f85fd3cfac
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-11 22:18:38 -0600

    Create ejercicios24.js

---
+++ ejercicios24.js
@@ -0,0 +1,6 @@
+public class NumeroAleatorio {
+    public static void main(String[] args) {
+        double aleatorio = Math.random();
+        System.out.println("Número aleatorio entre 0 y 1: " + aleatorio);
+    }
+}
