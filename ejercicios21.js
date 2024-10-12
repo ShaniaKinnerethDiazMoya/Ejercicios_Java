c21345dbbb12a55f60cd85ac2ae1e4902cfa8852
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-11 22:16:22 -0600

    Create ejercicios21.js

---
+++ ejercicios21.js
@@ -0,0 +1,9 @@
+public class ConcatenarCadenas {
+    public static void main(String[] args) {
+        String cadena1 = "Hola ";
+        String cadena2 = "Mundo!";
+        String resultado = cadena1 + cadena2;
+
+        System.out.println("Resultado de la concatenación: " + resultado);
+    }
+}
