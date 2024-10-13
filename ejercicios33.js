9dc85077bd037e60137a8ef4762a83b8a1b159b3
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-13 12:57:25 -0600

    Add files via upload

---
+++ ejercicios33.js
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = -10;
+        
+        if (numero < 0) {
+            System.out.println("El número es negativo.");
+        } else {
+            System.out.println("El número no es negativo.");
+        }
+    }
+}
