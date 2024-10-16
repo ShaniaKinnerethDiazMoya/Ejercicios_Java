d788b03c00ce41db953b2b518b1737aedbdebcdd
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-15 23:01:35 -0600

    Create ejercicios43.js

---
+++ ejercicios43.js
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 255;
+        String hexadecimal = Integer.toHexString(numero);
+        
+        System.out.println("El número " + numero + " en hexadecimal es: " + hexadecimal);
+    }
+}
