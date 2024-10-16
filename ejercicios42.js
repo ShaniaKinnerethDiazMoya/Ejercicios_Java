b294711d516036c7471f0cb8d494670b1b15371b
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-15 23:00:58 -0600

    Create ejercicios42.js

---
+++ ejercicios42.js
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 25;
+        String binario = Integer.toBinaryString(numero);
+        
+        System.out.println("El número " + numero + " en binario es: " + binario);
+    }
+}
