e034f11ee612bb71d6828cf234652ae737e020bd
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-18 20:22:03 -0600

    Create ejercicios57.js

---
+++ ejercicios57.js
@@ -0,0 +1,12 @@
+public class CompararCadenas {
+    public static void main(String[] args) {
+        String cadena1 = "Hola";
+        String cadena2 = "Hola";
+        
+        if (cadena1.equals(cadena2)) {
+            System.out.println("Las cadenas son iguales.");
+        } else {
+            System.out.println("Las cadenas no son iguales.");
+        }
+    }
+}
