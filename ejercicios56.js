5028e017eb11f0157aba006efc15fd848ddc35ba
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-18 20:21:28 -0600

    Create ejercicios56.js

---
+++ ejercicios56.js
@@ -0,0 +1,7 @@
+public class ValorAbsoluto {
+    public static void main(String[] args) {
+        int numero = -10;
+        int valorAbsoluto = Math.abs(numero);
+        System.out.println("El valor absoluto de " + numero + " es: " + valorAbsoluto);
+    }
+}
