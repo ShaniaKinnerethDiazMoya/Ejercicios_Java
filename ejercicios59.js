b66befd14c1fdd197ab221267e71990433f1206e
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-18 20:23:16 -0600

    Create ejercicios59.js

---
+++ ejercicios59.js
@@ -0,0 +1,8 @@
+public class ReemplazarCaracteres {
+    public static void main(String[] args) {
+        String cadena = "Java es divertido";
+        String nuevaCadena = cadena.replace('a', 'o');
+        System.out.println("Cadena original: " + cadena);
+        System.out.println("Cadena después de reemplazar: " + nuevaCadena);
+    }
+}
