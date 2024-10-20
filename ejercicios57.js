aa9b57b1b255daa22b436a2fb5072b53d723a146
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-19 21:53:31 -0600

    Add files via upload

--- ejercicios57.js
+++ ejercicios57.js
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class CompararCadenas {
     public static void main(String[] args) {
-        String cadena1 = "Hola";
-        String cadena2 = "Hola";
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce la primera cadena: ");
+        String cadena1 = sc.nextLine();
+        
+        System.out.print("Introduce la segunda cadena: ");
+        String cadena2 = sc.nextLine();
         
         if (cadena1.equals(cadena2)) {
             System.out.println("Las cadenas son iguales.");
