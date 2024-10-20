1798b31d91a0c1eb6acceee6c0646baba170aba9
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-10-19 21:54:05 -0600

    Add files via upload

--- ejercicios59.js
+++ ejercicios59.js
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class ReemplazarCaracteres {
     public static void main(String[] args) {
-        String cadena = "Java es divertido";
-        String nuevaCadena = cadena.replace('a', 'o');
-        System.out.println("Cadena original: " + cadena);
-        System.out.println("Cadena después de reemplazar: " + nuevaCadena);
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Introduce una cadena: ");
+        String cadena = sc.nextLine();
+        
+        System.out.print("Introduce el carácter a reemplazar: ");
+        char antiguoChar = sc.next().charAt(0);
+        
+        System.out.print("Introduce el carácter nuevo: ");
+        char nuevoChar = sc.next().charAt(0);
+        
+        String cadenaReemplazada = cadena.replace(antiguoChar, nuevoChar);
+        
+        System.out.println("Cadena resultante: " + cadenaReemplazada);
     }
 }
