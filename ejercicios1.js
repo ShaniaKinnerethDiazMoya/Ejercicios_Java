08a5167d872e5ba149307856e67bdf97e310e9e2
ShaniaKinnerethDiazMoya <160097502+ShaniaKinnerethDiazMoya@users.noreply.github.com>
2024-11-14 21:18:34 -0600

    Update ejercicios1.js

--- ejercicios1.js
+++ ejercicios1.js
@@ -2,3 +2,5 @@ let num1 = parseFloat(prompt("Ingresa el primer número:"));
 let num2 = parseFloat(prompt("Ingresa el segundo número:"));
 let suma = num1 + num2;
 console.log("La suma es: " + suma);
+
+
